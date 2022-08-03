4db6901b44b330bef0bfaf524b6a0e098370143c
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-04 00:17:31 +0500

    created GithubRepoDao

---
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/data/source/local/dao/GithubRepoDao.kt
@@ -0,0 +1,14 @@
+package com.shobu95.sadapay_takehomeexercise.data.source.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.shobu95.sadapay_takehomeexercise.data.source.local.entity.GithubRepo
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface GithubRepoDao {
+
+    @Query("SELECT * FROM github_repo_table")
+    fun getAll(): Flow<List<GithubRepo>>
+
+}
