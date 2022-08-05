edb9706a15d9bff6a7b6ffbac2d4c393cc8589bf
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-05 21:24:29 +0500

    updated return type of getTrendingRepos() as TrendingReposResponse.

--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/data/source/remote/services/GithubRepoService.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/data/source/remote/services/GithubRepoService.kt
@@ -1,8 +1,9 @@
 package com.shobu95.sadapay_takehomeexercise.data.source.remote.services
 
+import com.shobu95.sadapay_takehomeexercise.data.source.remote.response.TrendingReposResponse
 import retrofit2.http.GET
 
 interface GithubRepoService {
     @GET("search/repositories?q=language=+sort:stars")
-    suspend fun getTrendingRepos()
+    suspend fun getTrendingRepos(): TrendingReposResponse
 }
