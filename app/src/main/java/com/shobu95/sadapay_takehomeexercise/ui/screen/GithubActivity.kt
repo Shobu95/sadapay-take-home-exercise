3a33f292f254131e4a6db57323009561b4be402c
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-06 03:29:01 +0500

    Refactoring: MainActivity.kt renamed to GithubActivity.kt SadapayTakeHomeExerciseTheme renamed to GithubTheme

--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/GithubActivity.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/GithubActivity.kt
@@ -9,21 +9,21 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.shobu95.sadapay_takehomeexercise.ui.theme.SadapayTakeHomeExerciseTheme
+import com.shobu95.sadapay_takehomeexercise.ui.theme.GithubTheme
 
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    SadapayTakeHomeExerciseTheme {
+    GithubTheme {
     }
 }
 
-class MainActivity : ComponentActivity() {
+class GithubActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SadapayTakeHomeExerciseTheme {
+            GithubTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/theme/Theme.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/theme/Theme.kt
@@ -28,9 +28,9 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun SadapayTakeHomeExerciseTheme(
+fun GithubTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colors = if (darkTheme) {
         DarkColorPalette
