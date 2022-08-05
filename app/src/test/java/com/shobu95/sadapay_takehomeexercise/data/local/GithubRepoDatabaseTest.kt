61975625dd0c4cd1488c92c0565e42dda336dd4f
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-06 00:02:52 +0500

    created GithubRepoDatabaseTest.kt class for DB tests

---
+++ app/src/test/java/com/shobu95/sadapay_takehomeexercise/data/local/GithubRepoDatabaseTest.kt
@@ -0,0 +1,11 @@
+package com.shobu95.sadapay_takehomeexercise.data.local
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.FixMethodOrder
+import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
+
+@RunWith(AndroidJUnit4::class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class GithubRepoDatabaseTest {
+}
