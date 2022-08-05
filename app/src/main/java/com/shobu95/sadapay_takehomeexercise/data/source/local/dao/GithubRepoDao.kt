8354a521859dcca44cffe9e8b5eaea89d78f0768
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-05 23:46:44 +0500

    added method to bulk-insert a list of github repositories in GithubRepoDao interface.

--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/data/source/local/dao/GithubRepoDao.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/data/source/local/dao/GithubRepoDao.kt
@@ -1,6 +1,8 @@
 package com.shobu95.sadapay_takehomeexercise.data.source.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.shobu95.sadapay_takehomeexercise.data.source.local.entity.GithubRepoEntity
 import kotlinx.coroutines.flow.Flow
@@ -10,5 +12,8 @@ interface GithubRepoDao {
 
     @Query("SELECT * FROM github_repo_table")
     fun getAll(): Flow<List<GithubRepoEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(githubRepos: Flow<List<GithubRepoEntity>>)
 
 }
