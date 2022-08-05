d7efa88eecd2bfe6a33824aeb13d319bbff774d9
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-06 01:55:34 +0500

    Added Entity-to-Domain model mapping function in GithubRepoEntity.kt

--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/data/source/local/entity/GithubRepoEntity.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/data/source/local/entity/GithubRepoEntity.kt
@@ -2,6 +2,8 @@ package com.shobu95.sadapay_takehomeexercise.data.source.local.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.shobu95.sadapay_takehomeexercise.domain.model.GithubRepo
+import com.shobu95.sadapay_takehomeexercise.domain.model.RepoOwner
 
 @Entity(tableName = "github_repo_table")
 data class GithubRepoEntity(
@@ -14,3 +16,19 @@ data class GithubRepoEntity(
     var starsCount: Int,
     var avatar: String,
 )
+
+fun List<GithubRepoEntity>.asDomainModel(): List<GithubRepo> {
+    return map {
+        GithubRepo(
+            id = it.id,
+            repoName = it.repoName,
+            description = it.description,
+            language = it.language,
+            starCount = it.starsCount,
+            owner = RepoOwner(
+                name = it.authorName,
+                avatar = it.avatar
+            )
+        )
+    }
+}
