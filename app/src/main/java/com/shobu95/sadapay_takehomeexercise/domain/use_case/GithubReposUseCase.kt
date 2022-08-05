22dfed6ff48618786bfb376af54551094a4057b6
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-06 03:24:44 +0500

    Created GithubReposUseCase class for handling business logic/use cases.

---
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/domain/use_case/GithubReposUseCase.kt
@@ -0,0 +1,21 @@
+package com.shobu95.sadapay_takehomeexercise.domain.use_case
+
+import com.shobu95.sadapay_takehomeexercise.data.repository.GithubRepoRepository
+import com.shobu95.sadapay_takehomeexercise.domain.model.GithubRepo
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class GithubReposUseCase
+@Inject constructor(
+    private val repository: GithubRepoRepository,
+) {
+
+    fun getGithubRepos(): Flow<List<GithubRepo>> {
+        return repository.getAll()
+    }
+
+    suspend fun refreshGithubRepos() {
+        repository.refreshAll()
+    }
+
+}
