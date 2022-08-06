7471f02ece70c643d3b71f74946184a86fcce794
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-06 15:19:46 +0500

    Added ViewModel parent class and HiltViewModel annotation to GithubViewModel

--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/GithubViewModel.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/GithubViewModel.kt
@@ -2,15 +2,18 @@ package com.shobu95.sadapay_takehomeexercise.ui.screen
 
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
 import com.shobu95.sadapay_takehomeexercise.domain.use_case.GithubReposUseCase
 import com.shobu95.sadapay_takehomeexercise.ui.screen.state_event.GithubUiEvent
 import com.shobu95.sadapay_takehomeexercise.ui.screen.state_event.GithubUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class GithubViewModel
 @Inject constructor(
     val useCase: GithubReposUseCase,
-) {
+) : ViewModel() {
 
     private val _state = mutableStateOf(GithubUiState())
     val state: State<GithubUiState> = _state
