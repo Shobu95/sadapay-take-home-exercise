8b15a8868e2a34071897767fed8ca72a485cbb9c
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-06 14:59:39 +0500

    Added state and event handing to GithubViewModel

--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/GithubViewModel.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/GithubViewModel.kt
@@ -1,6 +1,10 @@
 package com.shobu95.sadapay_takehomeexercise.ui.screen
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import com.shobu95.sadapay_takehomeexercise.domain.use_case.GithubReposUseCase
+import com.shobu95.sadapay_takehomeexercise.ui.screen.state_event.GithubUiEvent
+import com.shobu95.sadapay_takehomeexercise.ui.screen.state_event.GithubUiState
 import javax.inject.Inject
 
 class GithubViewModel
@@ -8,5 +12,17 @@ class GithubViewModel
     val useCase: GithubReposUseCase,
 ) {
 
+    private val _state = mutableStateOf(GithubUiState())
+    val state: State<GithubUiState> = _state
 
+    fun onEvent(event: GithubUiEvent) {
+
+        when (event) {
+
+            GithubUiEvent.OnReload -> {
+
+            }
+        }
+
+    }
 }
