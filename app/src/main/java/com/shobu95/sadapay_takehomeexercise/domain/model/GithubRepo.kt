3f847cce74a835ef572276550ddd9c105eacf023
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-06 18:30:13 +0500

    Added nullable fields in GithubRepo.

--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/domain/model/GithubRepo.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/domain/model/GithubRepo.kt
@@ -3,13 +3,13 @@ package com.shobu95.sadapay_takehomeexercise.domain.model
 data class GithubRepo(
     val id: Int,
     val repoName: String,
-    val description: String,
-    val language: String,
+    val description: String?,
+    val language: String?,
     val starCount: Int,
     val owner: RepoOwner,
 )
 
 data class RepoOwner(
     val name: String,
-    val avatar: String,
+    val avatar: String?,
 )
