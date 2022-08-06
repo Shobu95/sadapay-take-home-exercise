20a80b3991035678889b290e80ddff933d5b800f
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-06 16:27:09 +0500

    Created refreshRepository function called refreshRepository function on the view-model's init function and onReload event.

--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/GithubViewModel.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/GithubViewModel.kt
@@ -1,31 +1,66 @@
 package com.shobu95.sadapay_takehomeexercise.ui.screen
 
-import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.shobu95.sadapay_takehomeexercise.domain.use_case.GithubReposUseCase
 import com.shobu95.sadapay_takehomeexercise.ui.screen.state_event.GithubUiEvent
 import com.shobu95.sadapay_takehomeexercise.ui.screen.state_event.GithubUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class GithubViewModel
 @Inject constructor(
-    val useCase: GithubReposUseCase,
+    private val useCase: GithubReposUseCase,
 ) : ViewModel() {
 
-    private val _state = mutableStateOf(GithubUiState())
-    val state: State<GithubUiState> = _state
+    var state by mutableStateOf(GithubUiState())
+        private set
+
+    init {
+        refreshDataInRepository()
+        useCase.getGithubRepos().map {
+            if (it.isNotEmpty()) {
+                state = state.copy(
+                    githubRepos = it,
+                    isLoading = false,
+                    isError = false
+                )
+            }
+
+        }
+    }
 
     fun onEvent(event: GithubUiEvent) {
-
         when (event) {
-
             GithubUiEvent.OnReload -> {
-
+                viewModelScope.launch {
+                    refreshDataInRepository()
+                }
             }
         }
+    }
 
+
+    private fun refreshDataInRepository() {
+        viewModelScope.launch {
+            state = try {
+                useCase.refreshGithubRepos()
+                state.copy(
+                    isLoading = true,
+                    isError = false,
+                )
+            } catch (ex: Exception) {
+                state.copy(
+                    isLoading = false,
+                    isError = true
+                )
+            }
+        }
     }
 }
