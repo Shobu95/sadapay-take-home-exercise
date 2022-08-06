6011c323646bd71d1cf60a1d48d06c37babeefa4
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-06 14:54:37 +0500

    GithubUiState converted to a data class for better state handling: State are: list, show-error and is-loading.

--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/state_event/GithubUiState.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/state_event/GithubUiState.kt
@@ -1,3 +1,9 @@
 package com.shobu95.sadapay_takehomeexercise.ui.screen.state_event
 
-sealed class GithubUiState
+import com.shobu95.sadapay_takehomeexercise.domain.model.GithubRepo
+
+data class GithubUiState(
+    val githubRepos: List<GithubRepo> = emptyList(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+)
