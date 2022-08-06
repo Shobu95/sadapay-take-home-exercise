dbc2e1f97dd1c6cc1233cba99d299017b2201c15
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-06 14:32:30 +0500

    created GithubViewModel

---
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/GithubViewModel.kt
@@ -0,0 +1,12 @@
+package com.shobu95.sadapay_takehomeexercise.ui.screen
+
+import com.shobu95.sadapay_takehomeexercise.domain.use_case.GithubReposUseCase
+import javax.inject.Inject
+
+class GithubViewModel
+@Inject constructor(
+    val useCase: GithubReposUseCase,
+) {
+
+
+}
