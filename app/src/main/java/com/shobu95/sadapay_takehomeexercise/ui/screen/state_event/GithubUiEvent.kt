671250c4ec7423c57aa599b604969a4678fa6051
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-07 23:09:23 +0500

    Added onToggleTheme event in GithubUiEvent class to handle events for toggling theme.

--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/state_event/GithubUiEvent.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/screen/state_event/GithubUiEvent.kt
@@ -1,5 +1,6 @@
 package com.shobu95.sadapay_takehomeexercise.ui.screen.state_event
 
 sealed class GithubUiEvent {
+    object OnToggleTheme : GithubUiEvent()
     object OnReload : GithubUiEvent()
 }
