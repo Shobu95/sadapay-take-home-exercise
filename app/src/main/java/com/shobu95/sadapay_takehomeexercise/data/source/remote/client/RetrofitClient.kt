41cc734f33c70c8da6ef4b35e829def7cfe50bf0
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-08 02:50:13 +0500

    Added OKHttp logger to RetrofitClient.kt

--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/data/source/remote/client/RetrofitClient.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/data/source/remote/client/RetrofitClient.kt
@@ -1,14 +1,26 @@
 package com.shobu95.sadapay_takehomeexercise.data.source.remote.client
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
+
     val retrofit: Retrofit by lazy {
         val BASE_URL = "https://api.github.com/"
 
+        val logger = HttpLoggingInterceptor()
+        logger.level = Level.BASIC
+
+        val loggerClient = OkHttpClient.Builder()
+            .addInterceptor(logger)
+            .build()
+
         Retrofit.Builder()
+            .client(loggerClient)
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
