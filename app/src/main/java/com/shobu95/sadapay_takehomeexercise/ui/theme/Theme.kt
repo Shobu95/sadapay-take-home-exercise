341e4a24a4facd892a3e84ef15def9c49f4be74e
Shoaib Ahmed <shoaib.ahmedsk95@gmail.com>
2022-08-08 04:00:50 +0500

    Added new colors for Theme in Color.kt and colors.xml.

--- app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/theme/Theme.kt
+++ app/src/main/java/com/shobu95/sadapay_takehomeexercise/ui/theme/Theme.kt
@@ -8,15 +8,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
+    primary = Blue200,
+    primaryVariant = Blue700,
     secondary = Teal200,
     background = Color.Black,
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
+    primary = Blue500,
+    primaryVariant = Blue700,
     secondary = Teal200,
     background = Color.White,
 
